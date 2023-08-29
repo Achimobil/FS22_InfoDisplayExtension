89cb46fa55dd64aafbbc333ae8ff8736e4afc94e
Achim Kuntz <achimobil@hotmail.com>
2023-08-29 20:04:03 +0200

    cmd korrekturen

--- Test.Ostsee.3.cmd
+++ Test.Ostsee.3.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files\7-Zip\7z.exe" a -tzip "E:\LS22Profile\Ostseekueste\mods\FS22_InfoDisplayExtension.zip" @FileDefinition.txt
-"C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 3 -cheats -profile E:\LS22Profile\Ostseekueste -devWarnings
+"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS22Profile\Ostseekueste\mods\FS22_InfoDisplayExtension.zip" @FileDefinition.txt
+start "" "C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 3 -cheats -profile E:\LS22Profile\Ostseekueste -devWarnings
