424c1b0a2caf1d276d1bba4f6b6c2a1244b6bfd2
Achim Kuntz <achimobil@hotmail.com>
2023-10-11 13:38:38 +0200

    remove cmds

--- Test.Modtest.3.cmd
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\7-Zip\7z.exe" u -tzip "E:\LS22Profile\LS22ModTest\mods\FS22_InfoDisplayExtension.zip" @FileDefinition.txt
-start "" "C:\Program Files (x86)\Farming Simulator 2022\x64\FarmingSimulator2022Game.exe" -autoStartSavegameId 3 -cheats -profile E:\LS22Profile\LS22ModTest -devWarnings
